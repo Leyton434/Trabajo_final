0ad51db38db6f3fd9f32c52d313d2163d5343cc7
Leyton434 <emanolpla2002@gmail.com>
2023-09-03 23:36:39 -0300

    Proyecto final

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Función para redirigir al hacer clic en el botón
+document.getElementById('boton').addEventListener('click', function() {
+    // Redirige a 'otra_pagina.html'
+    window.location.href = './pages/origen.html';
+});
+
